6f0bdb82d914632f956b738a9a74a00594e74096
Kunal Gautam <kunal@ikunal.in>
2015-03-01 20:59:23 +0530

    Assign value to variables.

--- variable/Variables.java
+++ variable/Variables.java
@@ -2,13 +2,21 @@ public class Variables
 {
 	public static void main(String[] args)
 	{
-	int number;
-	double rate;
+	int number = 100; /* declare here only */
+	double rate; /* will be declared later */
 	String firstName;
 	char letter;
 	boolean flag;
-
+	
 	int salary, hoursWorked;
 	String middleName, lastName, initials;
-	}
+	
+	rate = 3.14;
+	firstName = "Kunal"; /* Assign String variable in double quote; */
+	number = 25 * 5; //Reassign variable
+	letter = 'a'; // char value assing using single quote
+	flag = true;
+
+
+}
 }
