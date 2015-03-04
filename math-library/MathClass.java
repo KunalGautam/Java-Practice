ddb18daf098f426b4e75efe7c42185afaa09fb19
Kunal Gautam <kunal@ikunal.in>
2015-03-04 08:19:19 +0530

    Math library

---
+++ math-library/MathClass.java
@@ -0,0 +1,15 @@
+public class MathClass
+{
+	public static void main(String[] args)
+	{
+		int number = -100;
+		int numberA = 2;
+System.out.println(Math.abs(number)); // Absolute Value
+System.out.println(Math.sqrt(numberA)); // Sqare root
+System.out.println(Math.max(number, numberA)); //Find max
+System.out.println(Math.min(number, numberA)); //Find Minimum
+System.out.println(Math.pow(4,3)); // Power 4 Power Three
+System.out.println(Math.round(2.125563)); // Round number
+// More Math class field can be find out at http://docs.oracle.com/javase/7/docs/api/java/lang/Math.html / Or searching math library in java
+	}
+}
