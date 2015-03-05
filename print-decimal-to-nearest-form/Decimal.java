3880cbe74a868b5b674d6d21b0847ab23c2a6e4d
Kunal Gautam <kunal@ikunal.in>
2015-03-05 09:12:32 +0530

    Print decimal to nearest form.

---
+++ print-decimal-to-nearest-form/Decimal.java
@@ -0,0 +1,11 @@
+public class Decimal
+{
+	public static void main(String[] args)
+	{
+	double a = 22;
+	double b = 7;
+	double pi = a/b;
+	System.out.printf("pi = %.4f", pi); // 4f means 4 place after fload/decimal
+
+	}
+}
