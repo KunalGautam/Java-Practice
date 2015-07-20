7db1ccac34d6a999a44500bfcc5172bb5740061f
Kunal Gautam <kunal@ikunal.in>
2015-07-20 11:32:05 +0530

    Added more logical code. Initialized position as -1. If the search term is found, its position is set as i+1 (i == array index value). If it is found, break the loop. In case Value not found, position will be -1 only.

--- data-structure-and-algorithm/src/LinearSearch.java
+++ data-structure-and-algorithm/src/LinearSearch.java
@@ -1,5 +1,3 @@
-import java.lang.reflect.Array;
-
 /* Linear Search Algorithm */
 public class LinearSearch {
 
@@ -8,17 +6,22 @@ public class LinearSearch {
 	 */
 	public static void main(String[] args) {
 				
-		int[] arrayNum = {1,4,6,9,10,11}; // Declared a array in variable
-		int i, target; // Declaring target and loop variable.
-		target = 11; // Fixing target value to find
+		int[] arrayNum = {1,14,6,9,10,11}; // Declared a array in variable
+		int i, target, position = -1; // Declaring target, position and loop variable.
+		target = 1; // Fixing target value to find
 		for(i = 0; i < arrayNum.length; ++i){ // Loop start
 		    if (arrayNum[i] == target) {
-		    	System.out.print("The position of "+target+" is at "+ (i+1));
-		    	}
-		    else {
-		    	System.out.print("Unable to find the target value");
-		    }
+		    	position = i+1;
+		    	break;
+		    	} 
+		    
 		  }//Loop Ends
+		
+		if(position == -1){
+			System.out.print("Target value "+target+" was not found");
+		}else {
+	    	System.out.print("Target value "+target+" was found at position: "+position);
+	    }
 		  
 		
 
