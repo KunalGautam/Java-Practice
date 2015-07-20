9d79e6edf9dac7624fbf130c7199378a220f095b
Kunal Gautam <kunal@ikunal.in>
2015-07-20 10:31:25 +0530

    Linear Search Algo concept.

---
+++ data-structure-and-algorithm/src/LinearSearch.java
@@ -0,0 +1,24 @@
+import java.lang.reflect.Array;
+
+/* Linear Search Algorithm */
+public class LinearSearch {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+				
+		int[] arrayNum = {1,4,6,9,10,11}; // Declared a array in variable
+		int i, target; // Declaring target and loop variable.
+		target = 11; // Fixing target value to find
+		for(i = 0; i < arrayNum.length; ++i){ // Loop start
+		    if (arrayNum[i] == target) {
+		    	System.out.print("The position of "+target+" is at "+ (i+1));
+		    	}
+		  }//Loop Ends
+		  
+		
+
+	}
+
+}
